af24e2ee398c559e321afe7682cc6a8aca5cbacc
Andres <alexjar22@hotmail.com>
2018-12-13 00:07:06 -0500

    Agregado el DialogFragment para escoger la fecha de las citas

--- app/src/main/java/android/medicalist/com/medicalist/CitasFragment.java
+++ app/src/main/java/android/medicalist/com/medicalist/CitasFragment.java
@@ -1,20 +1,32 @@
 package android.medicalist.com.medicalist;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class CitasFragment extends Fragment {
+
+    final Calendar myCalendar = Calendar.getInstance();
+    EditText editText;
 
     public CitasFragment() {
         // Required empty public constructor
     }
 
-    public static CitasFragment newInstance(String param1, String param2) {
+    public static CitasFragment newInstance() {
         CitasFragment fragment = new CitasFragment();
         return fragment;
     }
@@ -32,6 +44,40 @@ public class CitasFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savBundle){
+        this.editText = (EditText) getView().findViewById(R.id.txtFecha);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        editText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                new DatePickerDialog(getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        this.editText.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    @Override
     public void onAttach(Context context) {
         super.onAttach(context);
     }
@@ -42,3 +88,5 @@ public class CitasFragment extends Fragment {
     }
 
 }
+
+
